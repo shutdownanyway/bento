eed5944e0421f178e6711c60a03ba15afa5db0f8
Ani <akereselidze@academic.rrc.ca>
2017-03-19 06:37:00 +0000

    Added index and show views for Product

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,9 @@ Rails.application.routes.draw do
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
 
-  resources :products
+  resources 'products', only: [:index, :show]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'products#index'
+  root to: 'products#index'
 
 
 end
