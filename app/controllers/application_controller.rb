2583c5e8ec4ebc208ca6999ece53b940e00d2485
Ani <akereselidze@academic.rrc.ca>
2017-04-10 01:03:28 +0000

    Fixed views and logout redirect for devise

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,14 +13,17 @@ class ApplicationController < ActionController::Base
     private
 
   # Overwriting the sign_out redirect path method
-  def after_sign_out_path_for(resource_or_scope)
-   if resource_or_scope == :user
-     new_user_session_path
-   elsif resource_or_scope == :admin
-     new_admin_session_path
-   else
-     root_path
-   end
-   end
+  # def after_sign_out_path_for(resource_or_scope)
+  #  if resource_or_scope == :user
+  #    new_user_session_path
+  #  elsif resource_or_scope == :admin
+  #    new_admin_session_path
+  #  else
+  #    root_path
+  #  end
+  #  end
 
+   def after_sign_out_path_for(resource_or_scope)
+    root_path
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users,  path: '', path_names: { sign_in: 'login', sign_out: 'logout'},  controllers: {
-        sessions: 'users/sessions'
-      }
+  devise_for :users,  path: '', path_names: { sign_in: 'login', sign_out: 'logout'}
   get 'category/index', to: 'category#index'
 
   get 'contact', to: 'contact#index', only: [:index]
