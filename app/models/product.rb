ac55ef247ddbd1bc65d0cf588696d9ad53f51c67
Ani <akereselidze@academic.rrc.ca>
2017-04-15 21:57:17 +0000

    Added image uploader funcitonality

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,7 +2,7 @@ class Product < ApplicationRecord
   has_many :line_items
   belongs_to :category
 
-
+    mount_uploader :image, ImageUploader
   # def self.keyword_search(keywords, category)
   #   keywords = "%" + keywords + "%"
   #   Product.where("name ILIKE ? OR description ILIKE ? AND category_id = ?", keywords, keywords, category)
