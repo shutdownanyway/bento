ac0713fa278f81273fb74387a3805eb56be84940
Ani <akereselidze@academic.rrc.ca>
2017-04-16 03:57:27 +0000

    Altering migration for heroku

--- db/migrate/20170416024415_fix_order_status_name.rb
+++ db/migrate/20170416024415_fix_order_status_name.rb
@@ -1,5 +1,5 @@
 class FixOrderStatusName < ActiveRecord::Migration[5.0]
   def change
-     rename_column :order_statuses, :name, :status
+     rename_column :order_statuses,  :status, :name
   end
 end
